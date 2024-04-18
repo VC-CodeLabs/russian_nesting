47438e1c8937d3761ecdd579a8a0de8c55414f11
Jeff Renshaw <jrenshaw@vividcloud.com>
2024-04-17 22:22:02 -0400

    JeffR: minimal timing test of {w,h} vs [w,h]
    compare the timing to read & write struct `{ int, int }` vs array `[2]int`
    
    since we can assign to either with `{ #, # }`, we can easily use either while preserving the ability to test with internal test samples
    
    on my system, writing is generally slower than reading (not too surprising), and a struct is generally faster on write while comparable on read, suggesting struct is the better choice vs array for single envelope def

---
+++ JeffR_RussianNesting_Solution.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+const DIM_MIN = 1
+
+var DIM_MAX = int(math.Pow(10, 5))
+
+const ENV_MIN = 1
+
+var ENV_MAX = DIM_MAX
+
+type EnvStruct struct {
+	width  int
+	height int
+}
+
+type EnvelopesS []EnvStruct
+
+type EnvArray [2]int
+
+type EnvelopesA [][2]int
+
+func main() {
+	testEnvelopeOps()
+}
+
+func start() time.Time {
+	return time.Now()
+}
+
+func finish(started time.Time) time.Duration {
+	return time.Since(started)
+}
+
+func assert(cond bool, msg string) {
+	if !cond {
+		panic(msg)
+	}
+}
+
+const TEST_RUNS = 10
+
+func testEnvelopeOps() {
+
+	testEnvelopesArrayOps()
+
+	testEnvelopesStructOps()
+
+}
+
+func testEnvelopesArrayOps() {
+
+	var wtt, rtt int64 = 0, 0
+	for r := 0; r < TEST_RUNS; r++ {
+		fmt.Println("Writing test [][w#,h#] array...")
+		ts := start()
+		envs := make(EnvelopesA, 0)
+		for i := 0; i < ENV_MAX; i++ {
+			envs = append(envs, EnvArray{i + 1, i + 1})
+
+		}
+		td := finish(ts)
+		fmt.Printf("Array with %d envelopes [w#,h#] made in %s\n", len(envs), td)
+		wtt += td.Nanoseconds()
+
+		fmt.Println("Reading test [][w#,h#] array...")
+		rts := start()
+		for i := 0; i < len(envs); i++ {
+			env := envs[i]
+			envW := env[0]
+			envH := env[1]
+			// be sure the read doesn't get optimized out!!!
+			if envW > DIM_MAX || envH > DIM_MAX {
+				fmt.Println("Never!!!")
+			}
+		}
+		rtd := finish(rts)
+		fmt.Printf("Array with %d envelopes [w#,h#] read in %s\n", len(envs), rtd)
+		rtt += rtd.Nanoseconds()
+	}
+	wta := time.Duration(float64(wtt/TEST_RUNS) * float64(time.Nanosecond))
+	rta := time.Duration(float64(rtt/TEST_RUNS) * float64(time.Nanosecond))
+	fmt.Printf("*** Average time to make [][w#,h#] %s read %s in %d runs\n", wta, rta, TEST_RUNS)
+}
+
+func testEnvelopesStructOps() {
+
+	var wtt, rtt int64 = 0, 0
+	for r := 0; r < TEST_RUNS; r++ {
+		fmt.Println("Writing test []{w#,h#} array...")
+		ts := start()
+		envs := make(EnvelopesS, 0)
+		for i := 0; i < ENV_MAX; i++ {
+			envs = append(envs, EnvStruct{i + 1, i + 1})
+
+		}
+		td := finish(ts)
+		fmt.Printf("Array with %d envelopes {w#,h#} made in %s\n", len(envs), td)
+		wtt += td.Nanoseconds()
+
+		fmt.Println("Reading test []{w#,h#} array...")
+		rts := start()
+		for i := 0; i < len(envs); i++ {
+			env := envs[i]
+			envW := env.width
+			envH := env.height
+			// be sure the read doesn't get optimized out!!!
+			if envW > DIM_MAX || envH > DIM_MAX {
+				fmt.Println("Never!!!")
+			}
+		}
+		rtd := finish(rts)
+		fmt.Printf("Array with %d envelopes {w#,h#} read in %s\n", len(envs), rtd)
+		rtt += rtd.Nanoseconds()
+
+	}
+	wta := time.Duration(float64(wtt/TEST_RUNS) * float64(time.Nanosecond))
+	rta := time.Duration(float64(rtt/TEST_RUNS) * float64(time.Nanosecond))
+	fmt.Printf("*** Average time to make []{w#,h#} %s read %s in %d runs\n", wta, rta, TEST_RUNS)
+
+	var envA = EnvelopesA{{1, 2}, {3, 4}}
+	var envS = EnvelopesS{{1, 2}, {3, 4}}
+
+	assert(len(envA) == len(envS), "mismatched lengths")
+}
