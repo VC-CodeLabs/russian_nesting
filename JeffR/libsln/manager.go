01b581baa835041c094661cac186f16ecc6493c7
Jeff Renshaw <jrenshaw@vividcloud.com>
2024-04-21 18:18:30 -0400

    JeffR: doc the manager

--- JeffR/libsln/manager.go
+++ JeffR/libsln/manager.go
@@ -1,8 +1,19 @@
 package librn
 
+// defines the high-level interface used in the main code;
+// abstracting at this level means I can easily swap in
+// a different impl for testing, while theoretically
+// having little or no significant impact on performance
 type IRussianNestingManager struct {
 	russianNestingImpl EnvArrayPreAlloc
+	// vs e.g. EnvArrayWithAppend
 }
+
+//
+// implement the RussianNesting interface for our manager;
+// note this just delegates to the underlying impl,
+// which must implement the same interface!
+//
 
 func (rnMgr *IRussianNestingManager) InitData() {
 	rnMgr.russianNestingImpl.InitData()
