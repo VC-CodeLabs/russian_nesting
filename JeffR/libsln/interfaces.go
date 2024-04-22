58075a706c57ac7e6f024fe31c0bce60c2f68377
Jeff Renshaw <jrenshaw@vividcloud.com>
2024-04-21 21:27:23 -0400

    JeffR: solution doc; add EnvCompact() to strip duplicates from input and use it

--- JeffR/libsln/interfaces.go
+++ JeffR/libsln/interfaces.go
@@ -1,13 +1,24 @@
 package librn
 
+//
+// defines an interface for the solution,
+// allowing for different impls to be swapped in and tested
+//
 type RussianNesting interface {
+	// prepare data storage
 	InitData()
+	// add a new envelope to data storage
 	PutDataItem(int, int)
+	// finalize data storage
 	CloseData()
+	// get the max nested envelopes collection
 	GetNestedEnvelopes() Envelopes
+	// get the count of max nested envelopes
 	GetNestedCount() int
 }
 
+// define an implementation of the solution
+// that uses a simple array
 type EnvArrayWithAppend struct {
 	envelopes Envelopes
 }
@@ -15,37 +26,44 @@ type EnvArrayWithAppend struct {
 func (x EnvArrayWithAppend) InitData() {
 }
 
+// appends a new envelope
 func (x *EnvArrayWithAppend) PutDataItem(w int, h int) {
 	(*x).envelopes = append(x.envelopes, Envelope{w, h})
 }
 
 func (x EnvArrayWithAppend) CloseData() {
-
+	// this method is intentionally empty
 }
 
 func (x EnvArrayWithAppend) GetNestedEnvelopes() Envelopes {
-	return EnvFilter(EnvSort(x.envelopes))
+	return EnvFilter(EnvCompact(EnvSort(x.envelopes)))
 }
 
 func (x EnvArrayWithAppend) GetNestedCount() int {
 	return len(x.GetNestedEnvelopes())
 }
 
+// define an implementation of the solution
+// that preallocates the data storage
+// rather than using append every time we add a new item
 type EnvArrayPreAlloc struct {
 	base      EnvArrayWithAppend
 	itemCount int
 }
 
+// preallocates data storage reserving space for ENV_MAX Envelope objects
 func (x *EnvArrayPreAlloc) InitData() {
 	(*x).base.envelopes = make(Envelopes, ENV_MAX)
 	// (*x).itemCount = 0
 }
 
+// adds new envelope to next available storage slot
 func (x *EnvArrayPreAlloc) PutDataItem(w int, h int) {
 	(*x).base.envelopes[x.itemCount] = Envelope{w, h}
 	(*x).itemCount++
 }
 
+// truncates the prealloc'd storage to what was actually written
 func (x *EnvArrayPreAlloc) CloseData() {
 	(*x).base.envelopes = x.base.envelopes[:x.itemCount]
 }
--- JeffR/libsln/solution.go
+++ JeffR/libsln/solution.go
@@ -6,6 +6,7 @@ import (
 	"slices"
 )
 
+// compare two envelopes to support sorting by size
 func envCmp(a Envelope, b Envelope) int {
 	diff := cmp.Compare(EnvWidth(a), EnvWidth(b))
 	if diff == 0 {
@@ -14,49 +15,98 @@ func envCmp(a Envelope, b Envelope) int {
 	return diff
 }
 
+// sort the collection and return it
 func EnvSort(envelopes Envelopes) Envelopes {
+	// TODO how expensive is this vs in-place sort?
 	slices.SortFunc(envelopes, envCmp)
 	return envelopes
 }
 
+// strip out the duplicates (must be sorted!!!)
+//
+// this significantly improves performance for
+// e.g. max # envelopes all matching size
+func EnvCompact(envelopes Envelopes) Envelopes {
+	return slices.Compact(envelopes)
+}
+
+// get the maximum # of nested envelopes in the specified collection;
+//
+// the input *MUST* be sorted and ideally compacted (duplicates removed) as well
 func EnvFilter(envelopes Envelopes) Envelopes {
 	lib.Assert(envelopes != nil && len(envelopes) > 0, "empty []Envelope collection, nothing to filter")
 
+	//
+	// the algorithm is fairly simple,
+	// once we've sorted and compacted the list;
+	// using each point in the list as a starting point,
+	// march thru the rest of the list,
+	// finding the next (smallest) envelope
+	// we can nest inside of,
+	// pulling the nested envelopes into a new collection
+	//
+	// at the end of each run,
+	// check to see if the nested collection
+	// is larger than the prior max,
+	// and if so, make it the new max
+	//
+
+	// track the max collection so far
 	var maxNestedEnvelopes Envelopes
 
-	// var nextEnv = Envelope{DIM_MAX + 1, DIM_MAX + 1}
+	// how many inputs are we dealing with
 	countOfEnvelopes := len(envelopes)
 
+	// walk thru the list of inputs, using each as
 	for i := range envelopes {
 
 		if i > 0 && len(maxNestedEnvelopes) > countOfEnvelopes-i {
-			// can't possibly find a bigger one, don't bother
+			// can't possibly find a bigger one, don't bother looking
 			break
 		}
 
+		// track the set of nested envelopes
 		filteredEnvelopes := make(Envelopes, 0)
+		// track the last envelope so we can see if it will it in the current one
 		var lastEnv = Envelope{-1, -1}
 		for j := i; j < countOfEnvelopes; j++ {
 			env := envelopes[j]
 			if j > i {
+				// not the first item, will the last envelope fit in this one?
 				if EnvWidth(env) > EnvWidth(lastEnv) && EnvHeight(env) > EnvHeight(lastEnv) {
-					// last envelope would fit inside the current one
+					// last envelope would fit inside the current one-
+					// add to the list of nested envelopes
 					filteredEnvelopes = append(filteredEnvelopes, lastEnv)
+					// ...and the current envelope becomes the new one
+					// we're trying to find a fit for;
+					// if we hit the end of input without
+					// finding a fit for this one,
+					// it gets tacked on below
 					lastEnv = env
 				}
 			} else {
+				// first item in this run,
+				// always becomes the new initial
+				// prior item for this run
 				lastEnv = env
 			}
+
+			// if we're at the end of this run, tack on the last containing envelope;
+			// this will either be the first envelope (because they all match)
+			// or the most recent envelope that could contain at least one that came before
 			if j == countOfEnvelopes-1 {
 				filteredEnvelopes = append(filteredEnvelopes, lastEnv)
 			}
 		}
 
+		// first pass or new larger nested collection?
 		if i == 0 || len(filteredEnvelopes) > len(maxNestedEnvelopes) {
+			// remember the new largest nested collection
 			maxNestedEnvelopes = filteredEnvelopes
 		}
 	}
 
+	// return the maximum nestable collection we found
 	return maxNestedEnvelopes
 
 }
